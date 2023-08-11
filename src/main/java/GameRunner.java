3f31a0c2568e706ec08eba6df8a9f16780595d8d
Steven Tran <StevenDoTran@gmail.com>
2023-08-11 14:38:34 -0500

    Testing shit out #1

--- src/main/java/GameRunner.java
+++ src/main/java/GameRunner.java
@@ -1,8 +1,35 @@
 import de.gurkenlabs.litiengine.Game;
+import de.gurkenlabs.litiengine.GameListener;
 
 public class GameRunner {
     public static void main(String args[]) {
         Game.init(args);
         Game.start();
+
+        Game.addGameListener(new GameListener() {
+            @Override
+            public void started() {
+                GameListener.super.started();
+                System.out.println("Game started");
+            }
+
+            @Override
+            public void initialized(String... args) {
+                GameListener.super.initialized(args);
+                System.out.println("Game initialized");
+            }
+
+            @Override
+            public boolean terminating() {
+                System.out.println("Game terminating");
+                return GameListener.super.terminating();
+            }
+
+            @Override
+            public void terminated() {
+                GameListener.super.terminated();
+                System.out.println("Game terminated");
+            }
+        });
     }
 }
